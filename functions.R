a0fe69c4e8f48e75e757a771ef1561d1120dbf1f
dahliasan <laninaesloco@gmail.com>
2024-09-28 07:59:48 +1000

    update functions

--- functions.R
+++ functions.R
@@ -1,41 +1,48 @@
+require(lubridate)
+require(dplyr)
+require(purrr)
+
+
 # Custom Functions ---------------------------------------------------------
 calcMeanDepartureDate <- function(dates, type = "median") {
-  dates_formatted <- purrr::map(jdates, function(x) {
-    ifelse(!month(x) %in% c(11, 12), format(x, '2000-%m-%d'), format(x, '1999-%m-%d'))
-  }) %>% unlist() %>% as.Date()
-  
-  if (type == "median") return(dates_formatted %>% median()) 
-  if (type == "mean") return (dates_formatted %>% mean()) 
-  if (type == "sd") return (dates_formatted %>% sd()) 
-}
+  dates_formatted <- purrr::map(dates, function(x) {
+    ifelse(!month(x) %in% c(11, 12), format(x, "2000-%m-%d"), format(x, "1999-%m-%d"))
+  }) %>%
+    unlist() %>%
+    as.Date()
 
-calcMeanDepartureDate <- function(dates, type = "median") {
-  dates_formatted <- purrr::map(jdates, function(x) {
-    ifelse(!month(x) %in% c(11, 12), format(x, '2000-%m-%d'), format(x, '1999-%m-%d'))
-  }) %>% unlist() %>% as.Date()
-  
-  if (type == "median") return(dates_formatted %>% median()) 
-  if (type == "mean") return (dates_formatted %>% mean()) 
-  if (type == "sd") return (dates_formatted %>% sd()) 
+  if (type == "median") {
+    return(dates_formatted %>% median())
+  }
+  if (type == "mean") {
+    return(dates_formatted %>% mean())
+  }
+  if (type == "sd") {
+    return(dates_formatted %>% sd())
+  }
 }
 
 saveDepartureDateResult <- function(results_table, dates, age_group) {
-  return (
-    bind_rows(results_table, 
-              tibble(age_group = age_group, 
-                     n =  dates %>% length,
-                     median = calcMeanDepartureDate(dates = ), 
-                     mean = calcMeanDepartureDate(dates, "mean"), 
-                     sd = calcMeanDepartureDate(dates, "sd"),
-                     median_yday = calcMeanDepartureDate(dates) %>% yday()))
+  return(
+    bind_rows(
+      results_table,
+      tibble(
+        age_group = age_group,
+        n = dates %>% length(),
+        median = calcMeanDepartureDate(dates = dates),
+        mean = calcMeanDepartureDate(dates, "mean"),
+        sd = calcMeanDepartureDate(dates, "sd"),
+        median_yday = calcMeanDepartureDate(dates) %>% yday()
+      )
+    )
   )
 }
 
 
 # dispersal bearing -------------------------------------------------------
 
-angle_diff <- function(theta1, theta2){
-  theta <- abs(theta1 - theta2) %% 360 
+angle_diff <- function(theta1, theta2) {
+  theta <- abs(theta1 - theta2) %% 360
   return(ifelse(theta > 180, 360 - theta, theta))
 }
 
@@ -46,12 +53,21 @@ eastOrWest <- function(theta) {
 
 whichZone <- function(theta) {
   theta <- theta %% 360
-  if(theta >= 0 & theta < 45) return("N-NE")
-  else if(theta >= 45 & theta < 90) return("NE-E")
-  else if(theta >= 90 & theta < 135) return("E-SE")
-  else if(theta >= 135 & theta < 180) return("SE-S")
-  else if(theta >= 180 & theta < 225) return("S-SW")
-  else if(theta >= 225 & theta < 270) return("SW-W")
-  else if(theta >= 270 & theta < 315) return("W-NW")
-  else return("NW-N")
+  if (theta >= 0 & theta < 45) {
+    return("N-NE")
+  } else if (theta >= 45 & theta < 90) {
+    return("NE-E")
+  } else if (theta >= 90 & theta < 135) {
+    return("E-SE")
+  } else if (theta >= 135 & theta < 180) {
+    return("SE-S")
+  } else if (theta >= 180 & theta < 225) {
+    return("S-SW")
+  } else if (theta >= 225 & theta < 270) {
+    return("SW-W")
+  } else if (theta >= 270 & theta < 315) {
+    return("W-NW")
+  } else {
+    return("NW-N")
+  }
 }
